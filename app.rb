820fd27c7cff9e9da36e49e2847fba857cbbc828
James Martinez <jamescmartinez@gmail.com>
2015-05-15 16:32:53 -0700

    Add redirects for previous pages

--- app.rb
+++ app.rb
@@ -3,3 +3,15 @@ require 'sinatra'
 get '/' do
   haml :index
 end
+
+get '/work' do
+  redirect to('#work')
+end
+
+get '/work/:x' do
+  redirect to('#work')
+end
+
+get '/consulting' do
+  redirect to('#hire')
+end
